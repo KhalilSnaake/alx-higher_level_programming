b9706c28c04176ebf15ce767ce9eb8bad2237dba
KhalilSnaake <khalilsang@gmail.com>
2022-12-01 22:07:50 +0000

    add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    count_arg = len(sys.argv)- 1
+    if count_arg == 0:
+        print("0 argument.")
+    elif count_argv == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count_arg))
+    for i in range(count_arg):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
